71dad7c021893ac58d422b4025408a6b9d847cde
alexander <alexander@debian.com>
2021-04-11 22:10:27 +0300

    Now code is normal. Added classes. Added readme.md. Added argparse library

---
+++ generate.py
@@ -0,0 +1,74 @@
+import argparse
+import getopt
+import os
+import re
+import shelve
+import sys
+
+from collections import OrderedDict
+from random import choices, choice
+
+
+class Generator3000:
+
+    def __init__(self):
+        pass
+
+    def read_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--seed',
+            type=str,
+            default = 'NULL_WRD',
+            help='Beginning word'
+        )
+        parser.add_argument(
+            '--model',
+            type=str,
+            default='model.py',
+            help='Path to the trained model file'
+        )
+        parser.add_argument(
+            '--length',
+            type=int,
+            default='5',
+            help='Length of generated sequence'
+        )
+        parser.add_argument(
+            '--output',
+            type=str,
+            default=False,
+            help='File which will be used to put generated sequence'
+        )
+
+        arguments = parser.parse_args()
+        self.seed = arguments.seed
+        self.length = arguments.length
+        self.model = arguments.model
+        self.output = arguments.output
+
+    def generate(self):
+        with shelve.open(self.model) as BIG_DICT:   
+            self.seed = self.seed if self.seed != 'NULL_WRD' else choice(list(BIG_DICT.keys()))
+            output = self.seed + " "
+            while self.length > 0:
+                vals = BIG_DICT[self.seed].values()
+                self.seed = choices(list(BIG_DICT[self.seed].keys()), weights=[i / sum(list(vals)) for i in list(vals)])[0]
+                
+                output += self.seed + " "
+                self.length -= 1
+
+        if self.output:
+            with open(self.output, 'w') as file:
+                file.write(output)
+        else:
+            print(output)
+
+def main():
+    generator = Generator3000()
+    generator.read_args()
+    generator.generate()
+
+
+if __name__ == "__main__":
+    main()
